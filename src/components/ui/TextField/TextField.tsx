63b1bc0184a19ee7fc5ebbca93b72ceda2620006
Ilya Shandulskiy <ilyashandulskiy@gmail.com>
2022-10-24 23:21:46 +0500

    feat: skeleton animation

--- src/components/ui/TextField/TextField.tsx
+++ src/components/ui/TextField/TextField.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './TextField.module.css';
+import cn from 'classnames';
 
 interface Props {
   label?: string;
@@ -8,25 +9,25 @@ interface Props {
 }
 
 const TextField = ({ label, text, tags }: Props) => {
-  if (text === undefined && !tags?.length)
-    return (
-      <div className={styles.container}>
-        {label && <div className={styles.skeletonLabel} />}
-        <div className={styles.skeletonText} />
-      </div>
-    );
+  const loading = text === undefined && !tags?.length;
 
   return (
     <div className={styles.container}>
-      {label && <p className={styles.label}>{label}</p>}
-      {text && <h5>{text}</h5>}
-      {tags && (
-        <div className={styles.tags}>
-          {tags.map((tag) => (
-            <div className={styles.tag}>{tag}</div>
-          ))}
-        </div>
-      )}
+      <div className={cn([styles.wrapper, { [styles.hide]: !loading }])}>
+        {label && <div className={styles.skeletonLabel} />}
+        <div className={styles.skeletonText} />
+      </div>
+      <div className={cn([styles.wrapper, { [styles.hide]: loading }])}>
+        {label && <p className={styles.label}>{label}</p>}
+        {text && <h5>{text}</h5>}
+        {tags && (
+          <div className={styles.tags}>
+            {tags.map((tag) => (
+              <div className={styles.tag}>{tag}</div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
